f37fb8bb88f5319ffba5057ce3123aae3180b614
SparshRaj7 <sparsh.21ug4003@iiitranchi.ac.in>
2024-08-22 19:23:04 +0530

    updated backend url

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
       email: data.email,
       password: data.password
     }
-    await axios.post("http://localhost:4001/user/login", userInfo)
+    await axios.post("https://bookstore-h5y3.onrender.com/user/login", userInfo)
     .then((res)=>{
       console.log(res.data);
       if(res.data){
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -23,7 +23,7 @@ function Signup() {
       email: data.email,
       password: data.password
     }
-    await axios.post("http://localhost:4001/user/signup", userInfo)
+    await axios.post("https://bookstore-h5y3.onrender.com/user/signup", userInfo)
     .then((res)=>{
       console.log(res.data);
       if(res.data){
